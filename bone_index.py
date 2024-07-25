43cb111b8ea1fbc2d3424c51bab21a4555fd691b
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-07-25 19:41:59 +0000

    Create bone_index.py

---
+++ bone_index.py
@@ -0,0 +1,29 @@
+bone_index = {
+    "head": 6,
+    "neck_0": 5,
+    "pelvis": 0,
+
+    "spine_1": 4,
+    "spine_2": 2,
+
+    "arm_upper_L": 8,
+    "arm_lower_L": 9,
+
+    "hand_L": 10,
+    "hand_R": 15,
+
+    "arm_upper_R": 13,
+    "arm_lower_R": 14,
+
+    "leg_upper_L": 22,
+    "leg_lower_L": 23,
+
+    "leg_upper_R": 25,
+    "leg_lower_R": 26,
+
+    "ankle_L": 24,
+    "ankle_R": 27,
+
+    "foot_L": 28,
+    "foot_R": 29,
+}
